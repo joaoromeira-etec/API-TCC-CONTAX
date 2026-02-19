4cdd178cb0266c56856e70aec54b39c6fee86754
Naiara "Kim <naiaraishiyacademico@gmail.com>
2026-02-19 19:10:48 -0300

    Update naiara.js

--- src/routes/naiara.js
+++ src/routes/naiara.js
@@ -3,24 +3,24 @@ const router = express.Router();
 
 const usuariosController = require('../controllers/usuarios');
 const empresasController = require('../controllers/empresas');
-const usuario_empresasController = require('../controllers/usuario_empresas');
+const usuarioEmpresaController = require('../controllers/usuarioEmpresa');
 
 router.get('/usuarios', usuariosController.listarUsuarios);
 router.post('/usuarios', usuariosController.cadastrarUsuarios);
 router.patch('/usuarios/:id', usuariosController.editarUsuarios);
 router.delete('/usuarios/:id', usuariosController.apagarUsuarios);
-//router.delete('/usuarios/del/:id', usuariosController.ocultarUsuarios);
+router.delete('/usuarios/del/:id', usuariosController.ocultarUsuarios);
 
 router.get('/empresas', empresasController.listarEmpresas);
 router.post('/empresas', empresasController.cadastrarEmpresas);
 router.patch('/empresas/:id', empresasController.editarEmpresas);
 router.delete('/empresas/:id', empresasController.apagarEmpresas);
-//router.delete('/empresas/del/:id', usuariosController.ocultarEmpresas);
+router.delete('/empresas/del/:id', empresasController.ocultarEmpresas);
 
-router.get('/usuario_empresas', usuario_empresasController.listarUsuarioEmpresa);
-router.post('/usuario_empresas', usuario_empresasController.cadastrarUsuarioEmpresa);
-router.patch('/usuario_empresas/:id', usuario_empresasController.editarUsuarioEmpresa);
-router.delete('/usuario_empresas/:id', usuario_empresasController.apagarUsuarioEmpresa);
-//router.delete('/usuario_empresas/del/:id', usuario_empresasController.ocultarUsuarioEmpresa);
+router.get('/usuario_empresas', usuarioEmpresaController.listarUsuarioEmpresa);
+router.post('/usuario_empresas', usuarioEmpresaController.cadastrarUsuarioEmpresa);
+router.patch('/usuario_empresas/:id', usuarioEmpresaController.editarUsuarioEmpresa);
+router.delete('/usuario_empresas/:id', usuarioEmpresaController.apagarUsuarioEmpresa);
+router.delete('/usuario_empresas/del/:id', usuarioEmpresaController.ocultarUsuarioEmpresa);
 
 module.exports = router;
