17a887af4c2ed55275d35b38deb00a9e760be191
Naiara "Kim <naiaraishiyacademico@gmail.com>
2026-05-12 20:13:51 -0300

    atualização suporte

--- src/routes/joaoPedro.js
+++ src/routes/joaoPedro.js
@@ -4,7 +4,6 @@ const router = express.Router();
 // Importe dos controllers
 const TipoDocumentosController = require('../controllers/tipoDocumentos');
 const DocumentosController = require('../controllers/documentos');
-const SuporteController = require('../controllers/suporte');
 const RegimeEmpresaController = require('../controllers/regimeEmpresa');
 
 // Rotas para Tipo de Documentos
@@ -21,13 +20,6 @@ router.post('/documentos', DocumentosController.cadastrarDocumentos);
 router.patch('/documentos/:id', DocumentosController.editarDocumentos);
 router.delete('/documentos/:id', DocumentosController.apagarDocumentos); //Não-Recomendado.
 router.delete('/documentos/del/:id', DocumentosController.ocultarDocumentos); //Recomendado.
-
-
-// Rotas para Suporte
-router.get('/suporte', SuporteController.listarSuportes);
-router.post('/suporte', SuporteController.cadastrarSuporte);
-router.patch('/suporte/:id', SuporteController.editarSuporte);
-router.delete('/suporte/:id', SuporteController.apagarSuporte);
 
 // Rotas para Regime da Empresa
 router.get('/regimeEmpresa', RegimeEmpresaController.listarRegimeEmpresa);
