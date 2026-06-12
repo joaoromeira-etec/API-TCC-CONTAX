a00bc9959089038735235ff3dd3430ccb473a039
Naiara "Kim <naiaraishiyacademico@gmail.com>
2026-06-12 20:38:37 -0300

    começo de upload

---
+++ uploads/uploadHelper.js
@@ -0,0 +1,10 @@
+const multer = require('multer');
+const fs = require('fs');
+
+//Função que cria a configuração do multer dinamicamente
+const uploadImage = (destinationFolder) => {
+    //Validação para garantir que a pasta de destino exista
+    if (!destinationFolder) {
+        throw new Error("A pasta de destino é obrigatória para o upload.");
+}
+}
