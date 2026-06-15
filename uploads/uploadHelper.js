92cb04e2f43fad9f88e5f2597650b99d1513704e
Naiara-Ishiy <naiaraishiyacademico@gmail.com>
2026-06-15 19:38:58 -0300

    arquivo que ajuda a fazer upload de imagem

--- uploads/uploadHelper.js
+++ uploads/uploadHelper.js
@@ -7,5 +7,39 @@ const uploadImage = (destinationFolder) => {
     if (!destinationFolder) {
         throw new Error("A pasta de destino é obrigatória para o upload.");
 }
+    const fullPath = `./uploads/${destinationFolder}/`;
+    if (!fs.existsSync(fullPath)) {
+        fs.mkdirSync(fullPath, { recursive: true });
+    }
 
+    //Configuração do storage (onde e como salvar)
+    const storage = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, fullPath);
+        },
+        filename: function (req, file, cb) {
+            const uniqueSuffic = Date.now + '-' + Math.round(Math.random() * 1E9);
+            //Extrai a extensão do arquivo a partir do mimetype
+            const extension = file.mimetype.split('/')[1];
+            cb(null, `${uniqueSuffic}.${extension}`);
+        }
+    });
+
+    //Filtro para aceitar apenas certos tipos de imagem
+    const fileFilter = (req, file, cb) => {
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/gif') {
+            cb(null, true);
+        } else {
+            cb(new Error('Apenas arquivos JPEG, PNG e GIF são permitidos.'), false);
+        }
+    };
+
+    return multer ({
+        storage: storage,
+        limits: {
+            fileSize: 1024 * 1024 * 5 // 5MB
+        },
+        fileFilter: fileFilter
+    });
 }
+module.exports = uploadImage;
